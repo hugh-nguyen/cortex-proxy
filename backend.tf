6305660cfb328e76d2c557d04521fac6c8cf5181
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-02-27 15:46:06 +1100

    test

--- backend.tf
+++ backend.tf
@@ -1,5 +1,9 @@
 terraform {
-  backend "local" {
-    path = "./terraform.tfstate"
+  backend "s3" {
+    bucket         = "mapleharbour-tf-state"
+    key            = "envoy-service-hub/terraform.tfstate"
+    region         = "ap-southeast-2"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
   }
 }

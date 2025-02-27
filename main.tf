d9c49bdb7e6d4be18d64794e8f3b13413a776f62
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-02-28 10:33:15 +1100

    nat

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_vpc" "eks_vpc" {
   }
 }
 
-resource "aws_subnet" "eks" {
+resource "aws_subnet" "public" {
   count = 2
 
   vpc_id                  = aws_vpc.eks_vpc.id
@@ -45,7 +45,21 @@ resource "aws_subnet" "eks" {
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
 
   tags = {
-    Name = "eks-subnet-${count.index}"
+    Name = "eks-public-subnet-${count.index}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = 2
+
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, count.index + 2)
+  map_public_ip_on_launch = false
+
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+
+  tags = {
+    Name = "eks-private-subnet-${count.index}"
   }
 }
 
@@ -57,6 +71,21 @@ resource "aws_internet_gateway" "eks_igw" {
   tags = {
     Name = "eks-internet-gateway"
   }
+}
+
+resource "aws_nat_gateway" "eks_nat" {
+  allocation_id = aws_eip.eks_nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "eks-nat-gateway"
+  }
+
+  depends_on = [aws_internet_gateway.eks_igw]
+}
+
+resource "aws_eip" "eks_nat" {
+  domain = "vpc"
 }
 
 resource "aws_route_table" "public" {
@@ -72,10 +101,29 @@ resource "aws_route_table" "public" {
   }
 }
 
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.eks_nat.id
+  }
+
+  tags = {
+    Name = "eks-private-route-table"
+  }
+}
+
 resource "aws_route_table_association" "public" {
   count = 2
-  subnet_id      = aws_subnet.eks[count.index].id
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count = 2
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
 }
 
 resource "aws_eks_cluster" "eks" {
@@ -83,7 +131,7 @@ resource "aws_eks_cluster" "eks" {
   role_arn = aws_iam_role.eks.arn
 
   vpc_config {
-    subnet_ids             = aws_subnet.eks[*].id
+    subnet_ids             = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
     endpoint_private_access = true  
     endpoint_public_access  = true
   }
@@ -114,7 +162,7 @@ resource "aws_eks_fargate_profile" "default" {
   fargate_profile_name   = "default"
   pod_execution_role_arn = aws_iam_role.fargate.arn
 
-  subnet_ids = aws_subnet.eks[*].id
+  subnet_ids = aws_subnet.private[*].id
 
   selector {
     namespace = "default"
@@ -125,7 +173,7 @@ resource "aws_eks_fargate_profile" "kube_system" {
   cluster_name           = aws_eks_cluster.eks.name
   fargate_profile_name   = "kube-system"
   pod_execution_role_arn = aws_iam_role.fargate.arn
-  subnet_ids = aws_subnet.eks[*].id
+  subnet_ids = aws_subnet.private[*].id
 
   selector {
     namespace = "kube-system"
@@ -136,7 +184,7 @@ resource "aws_eks_fargate_profile" "envoy_gateway" {
   cluster_name           = aws_eks_cluster.eks.name
   fargate_profile_name   = "envoy-gateway"
   pod_execution_role_arn = aws_iam_role.fargate.arn
-  subnet_ids = aws_subnet.eks[*].id
+  subnet_ids = aws_subnet.private[*].id
 
   selector {
     namespace = "envoy-gateway-system"
